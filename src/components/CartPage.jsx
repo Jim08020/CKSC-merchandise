e4757e678431cc63b703f3255c6a4e179a9e2cbd
ChrisSun0731 <chris20090731@gmail.com>
2025-08-26 22:19:04 +0800

    css

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -50,7 +50,8 @@ export default function CartPage() {
       minHeight: "100vh",
       padding: "40px 20px",
       display: "flex",
-      justifyContent: "center"
+      justifyContent: "center",
+      alignItems: "flex-start",
     }}>
       <div style={{
         width: "100%",
